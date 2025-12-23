75ea8b10c7202441e2a72cef759b0426a307024d
Tior931108 <jyj8469@gmail.com>
2025-12-23 10:38:15 +0900

    ♻ refactor: 3. 코드 개선 퀴즈 - JPA의 이해
    - 할일 검색시 weather 조건으로도 검색할 수 있도록 추가
    - 할 일 검색시 수정일 기준으로 기간 검색이 가능하도록 추가
    - 추가한 조건은 필수가 아니기에 required = false 적용

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,15 +7,26 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
             "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+
+    // 할 일 검색시 weather 조건으로도 검색하거나 수정일 기준 기간의 시작과 끝 검색하도록 JPQL 수정
+    @Query("SELECT t FROM Todo t " +
+            "WHERE (:weather IS NULL OR t.weather = :weather) " +
+            // modifiedAt이 LocalDateTime형식이어서 LocalDate형식 맞추기 위해 JPQL에서 DATE() 함수 사용
+            "AND (:startDate IS NULL OR DATE(t.modifiedAt) >= :startDate) " +
+            "AND (:endDate IS NULL OR DATE(t.modifiedAt) <= :endDate)")
+    Page<Todo> searchTodos(
+            @Param("weather") String weather,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            Pageable pageable
+    );
 }
